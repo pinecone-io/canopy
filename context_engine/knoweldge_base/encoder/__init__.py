2bab6eb136bce20ed7a7011fa6633e1f495ce3de
ilai <ilai@pinecone.io>
2023-08-14 15:36:43 +0300

    [kb] Expose encoders at the sub-module level

--- context_engine/knoweldge_base/encoder/__init__.py
+++ context_engine/knoweldge_base/encoder/__init__.py
@@ -1,1 +1,3 @@
 from .pipeline_encoder import PipelineEncoder
+from .dense import DenseEncodingStep
+from .hybrid import HybridEncodingStep
--- context_engine/knoweldge_base/encoder/steps/__init__.py
+++
@@ -1,7 +0,0 @@
-from .dense import DenseEncodingStep
-from .hybrid import HybridEncodingStep
-
-__all__ = [
-    "DenseEncodingStep",
-    "HybridEncodingStep",
-]
