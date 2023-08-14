d07144a4b08e064fe09ee1722c4436ab926b0c9e
ilai <ilai@pinecone.io>
2023-08-14 15:33:51 +0300

    [kb] Changed existing EncoderSteps to Encoders
    We don't want `Steps` anymore, but simple Encoders (and pipeline)

--- context_engine/knoweldge_base/encoder/steps/hybrid.py
+++ context_engine/knoweldge_base/encoder/steps/hybrid.py
@@ -1,24 +1,28 @@
 from typing import List
 
 from pinecone_text.hybrid import hybrid_convex_scale
+from pinecone_text.dense.base_dense_ecoder import BaseDenseEncoder
 from pinecone_text.sparse.base_sparse_encoder import BaseSparseEncoder
 
-from context_engine.knoweldge_base.encoder.steps.base import Encoder
+from context_engine.knoweldge_base.encoder.base import Encoder
 from context_engine.knoweldge_base.models import KBQuery
 
 
 class HybridEncodingStep(Encoder):
 
-    def __init__(self, sparse_encoder: BaseSparseEncoder, default_alpha: float = 0.5):
+    def __init__(self,
+                 dense_encoder: BaseDenseEncoder,
+                 sparse_encoder: BaseSparseEncoder,
+                 default_alpha: float = 0.5):
+        self.dense_encoder = dense_encoder
         self.sparse_encoder = sparse_encoder
         self.default_alpha = default_alpha
 
-    def encode_queries(self, queries: List[KBQuery]):
+    def _encode_queries_batch(self, queries: List[KBQuery]):
         # NOTE: assumes that a DenseEncodingStep was run first
-        for query in queries:
-            if query.values is None:
-                raise ValueError("Must run DenseEncodingStep before HybridEncodingStep")
+        values = self.dense_encoder.encode_query([q.text for q in queries])
+        sparse_values = self.sparse_encoder.encode_query([q.text for q in queries])
 
+        for query, val, sparse_val in zip(queries, values, sparse_values):
             alpha = query.query_params.get("alpha", self.default_alpha)
-            query.sparse_values = self.sparse_encoder.encode_query(query.text)
-            query.values, query.sparse_values = hybrid_convex_scale(query.values, query.sparse_values, alpha)
+            query.values, query.sparse_values = hybrid_convex_scale(val, sparse_val, alpha)
