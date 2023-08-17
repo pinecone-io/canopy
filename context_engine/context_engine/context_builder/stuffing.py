4723da582347cdd3c05e07bb655f242da5c8fe77
Amnon Catav <catav.amnon1@gmail.com>
2023-08-17 14:56:15 +0300

    finish first implementation

--- context_engine/context_engine/context_builder/stuffing.py
+++ context_engine/context_engine/context_builder/stuffing.py
@@ -12,41 +12,38 @@ class StuffingContextBuilder(BaseContextBuilder):
 
     def __init__(self, tokenizer: Tokenizer, reference_metadata_field: str):
         self._tokenizer = tokenizer
-        self._source_metadata_field = source_metadata_field
-        self._include_queries = include_queries
+        self._reference_metadata_field= reference_metadata_field
 
     def build(self, query_results: List[QueryResult], max_context_tokens: int) -> Context:
-        # Initial variables
-        num_tokens = 0
+
         sorted_docs_with_origin = self._round_robin_sort(query_results)
 
         # Stuff as many documents as possible into the context
         context_query_results: List[ContextQueryResult] = [ContextQueryResult(query=qr.query, snippets=[])
                                                            for qr in query_results]
         debug_info = {"num_docs": len(sorted_docs_with_origin)}
-        context = Context(content=context_query_results, num_tokens=num_tokens, debug_info=debug_info)
-        inserted_doc_ids = set()
+        context = Context(content=context_query_results, num_tokens=0, debug_info=debug_info)
+
+        if self._tokenizer.token_count(context.to_text()) > max_context_tokens:
+            return Context(content="", num_tokens=0, debug_info=debug_info)
+
+        seen_doc_ids = set()
         for doc, origin_query_idx in sorted_docs_with_origin:
-            doc_tokens = self._tokenizer.token_count(doc.text)
-            if (num_tokens + doc_tokens <= max_context_tokens) and doc.id not in inserted_doc_ids:
-                snippet = ContextSnippet(text=doc.text, source=doc.metadata.get(self._source_metadata_field, None))
+            if doc.id not in seen_doc_ids:
+                snippet = ContextSnippet(text=doc.text, source=doc.metadata.get(self._reference_metadata_field, None))
+
+                # try inserting the snippet into the context
                 context_query_results[origin_query_idx].snippets.append(snippet)
-                num_tokens += doc_tokens
-                inserted_doc_ids.add(doc.id)
+                seen_doc_ids.add(doc.id)
+                # if the context is too long, remove the snippet
+                if self._tokenizer.token_count(context.to_text()) > max_context_tokens:
+                    context_query_results[origin_query_idx].snippets.pop()
+
+        context.num_tokens = self._tokenizer.token_count(context.to_text())
         return context
 
     @staticmethod
     def _round_robin_sort(query_results: List[QueryResult]) -> List[Tuple[DocumentWithScore, int]]:
-        """
-        Sort documents from query results in a round-robin manner.
-
-        Args:
-            query_results (List[QueryResult]): The list of query results containing documents.
-
-        Returns:
-            List[Tuple[DocumentWithScore, int]]: List of tuples where each tuple contains a document
-                                                 and its origin index.
-        """
         sorted_docs_with_origin = []
 
         for docs_tuple in zip_longest(*[qr.documents for qr in query_results], fillvalue=None):
