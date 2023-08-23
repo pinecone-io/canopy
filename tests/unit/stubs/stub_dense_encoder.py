c8c90f2936a2860d8ecaad789aec5d7bf8fb4590
Amnon Catav <catav.amnon1@gmail.com>
2023-08-23 18:46:05 +0300

    kb with system tests

--- tests/unit/stubs/stub_dense_encoder.py
+++ tests/unit/stubs/stub_dense_encoder.py
@@ -1,4 +1,5 @@
 import hashlib
+import numpy as np
 from typing import Union, List
 
 from pinecone_text.dense.base_dense_ecoder import BaseDenseEncoder
@@ -19,17 +20,23 @@ class StubDenseEncoder(BaseDenseEncoder):
                        ) -> Union[List[float], List[List[float]]]:
         return self._encode(texts)
 
-    @staticmethod
-    def consistent_hash_float(text: str) -> float:
-        sha256_hash = hashlib.sha256(text.encode()).hexdigest()
-        int_value = int(sha256_hash, 16)
-        # Normalize the integer value to a float in [0, 1)
-        return int_value / float(1 << 256)
+    def consistent_embedding(self, text: str) -> List[float]:
+        # consistent embedding function that project each text to a unique angle
+        embedding = []
+        for i in range(self.dimension):
+            sha256_hash = hashlib.sha256((text + str(i)).encode()).hexdigest()
+            int_value = int(sha256_hash, 16)
+            embedding.append(int_value / float(1 << 256))
+
+        l2_norm = np.linalg.norm(embedding)
+        normalized_embedding = [float(value / l2_norm) for value in embedding]
+
+        return normalized_embedding
 
     def _encode(self,
                 texts: Union[str, List[str]]
                 ) -> Union[List[float], List[List[float]]]:
         if isinstance(texts, str):
-            return [self.consistent_hash_float(texts)] * self.dimension
+            return self.consistent_embedding(texts)
         else:
-            return [[self.consistent_hash_float(t)] * self.dimension for t in texts]
+            return [self.consistent_embedding(text) for text in texts]
