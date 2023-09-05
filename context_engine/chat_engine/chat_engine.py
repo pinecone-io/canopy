889aedb71cbdd0646538cfd1a9e2e30cbeb35b98
ilai <ilai@pinecone.io>
2023-09-06 01:23:53 +0300

    [chat] Revert accidental change to prompt

--- context_engine/chat_engine/chat_engine.py
+++ context_engine/chat_engine/chat_engine.py
@@ -12,15 +12,10 @@ from context_engine.llm.models import ModelParams, SystemMessage
 from context_engine.models.api_models import StreamingChatResponse, ChatResponse
 from context_engine.models.data_models import Context, Messages
 
-DEFAULT_SYSTEM_PROMPT = """"Use the following pieces of context to answer the user 
-question at the next messages. This context retrieved from a knowledge database and 
-you should use only the facts from the context to answer. Always remember to include 
-the reference to the documents you used from their 'reference' field in the format 
-'Source: $REFERENCE_HERE'.
-If you don't know the answer, just say that you don't know, don't try to make up an 
-answer, use the context."
-Don't address the context directly, but use it to answer the user question like it's 
-your own knowledge."""  # noqa
+
+DEFAULT_SYSTEM_PROMPT = """"Use the following pieces of context to answer the user question at the next messages. This context retrieved from a knowledge database and you should use only the facts from the context to answer. Always remember to include the reference to the documents you used from their 'reference' field in the format 'Source: $REFERENCE_HERE'.
+If you don't know the answer, just say that you don't know, don't try to make up an answer, use the context."
+Don't address the context directly, but use it to answer the user question like it's your own knowledge."""  # noqa
 
 
 class BaseChatEngine(ABC):
