16234dd72b77e07fc99414b2cc79e0d03606829d
Amnon Catav <catav.amnon1@gmail.com>
2023-09-10 15:53:57 +0300

    remove langchain dependency

--- tests/unit/chunker/test_markdown_chunker.py
+++ tests/unit/chunker/test_markdown_chunker.py
@@ -1,6 +1,6 @@
 import pytest
-from context_engine.knoweldge_base.chunker.recursive_character \
-    import RecursiveCharacterChunker
+
+from context_engine.knoweldge_base.chunker import MarkdownChunker
 from context_engine.knoweldge_base.models import KBDocChunk
 from context_engine.models.data_models import Document
 from tests.unit.chunker.base_test_chunker import BaseTestChunker
@@ -11,13 +11,19 @@ class TestMarkdownChunker(BaseTestChunker):
     @staticmethod
     @pytest.fixture(scope="class")
     def chunker():
-        return RecursiveCharacterChunker(chunk_size=40,
-                                         chunk_overlap=30)
+        return MarkdownChunker(chunk_size=56,
+                               chunk_overlap=10)
 
     @staticmethod
     @pytest.fixture
     def text():
-        return """# Markdown Example for Unit Testing
+        long_text = ("In a sleepy village, Lay a hidden secret. Anna found a map, "
+                     "Old, torn, and creased. It marked a spot, Beneath the old oak. "
+                     "With shovel in hand, Anna began to dig. Hours turned to minutes, "
+                     "Then, a chest appeared. Golden light spilled out, The village's "
+                     "lost lore. Inside, not gold, But memories and tales. Of brave "
+                     "ancestors, And magical whales.")
+        return f"""# Markdown Example for Unit Testing
 
 ## Headers
 ### Level 3
@@ -70,6 +76,15 @@ text after first level header
 
 > This is a blockquote.
 
+## long text
+{long_text}
+
+Inside, not gold, But memories and tales.
+Of brave ancestors, And magical whales.
+
+Anna shared the stories, Under stars so bright.
+The village united, Bathed in tales' light.
+
 ## Inline code
 
 Here's some inline `code`.
@@ -104,40 +119,98 @@ a | b | c
     @staticmethod
     @pytest.fixture
     def expected_chunks(documents):
-        return [
-            KBDocChunk(id='test_document_1_0',
-                       text='# Markdown Example for Unit Testing\n\n## Headers\n### Level 3\ntext in level 3\n#### Level 4\ntext in level 4\n##### Level 5\ntext in level 5\n###### Level 6\ntext in level 6\n\n## Emphasis',  # noqa: E501
-                       metadata={'test': '1'},
-                       document_id='test_document_1'),
-            KBDocChunk(id='test_document_1_1',
-                       text='## Emphasis\n\n*Italic text* or _Italic text_\n\n**Bold text** or __Bold text__\n\n**_Bold and italic_** or *__Bold and italic__*\n\n~~Strikethrough text~~\n\n# Another first level header\ntext after first level header\n\n## Lists\n\n### Unordered',  # noqa: E501
-                       metadata={'test': '1'},
-                       document_id='test_document_1'),
-            KBDocChunk(id='test_document_1_2',
-                       text='**Bold text** or __Bold text__\n\n**_Bold and italic_** or *__Bold and italic__*\n\n~~Strikethrough text~~\n\n# Another first level header\ntext after first level header\n\n## Lists\n\n### Unordered\n\n* Item 1\n* Item 2\n  * Sub-item 2.1\n  * Sub-item 2.2',  # noqa: E501
-                       metadata={'test': '1'},
-                       document_id='test_document_1'),
+        chunks = [
+            KBDocChunk(
+                id='test_document_1_0',
+                text='# Markdown Example for Unit Testing\n\n## Headers\n### Level 3'
+                     '\ntext in level 3\n#### Level 4\ntext in level 4\n##### Level 5'
+                     '\ntext in level 5\n###### Level 6\ntext in level 6',
+                source='',
+                metadata={'test': '1'},
+                document_id='test_document_1'),
+
+            KBDocChunk(
+                id='test_document_1_1',
+                text='## Emphasis\n\n*Italic text* or _Italic text_'
+                     '\n\n**Bold text** or '
+                     '__Bold text__'
+                     '\n\n**_Bold and italic_** or *__Bold and italic__*\n\n'
+                     '~~Strikethrough text~~',
+                     source='',
+                     metadata={'test': '1'},
+                     document_id='test_document_1'),
+
+            KBDocChunk(
+                id='test_document_1_2',
+                text='# Another first level header\ntext after first level header'
+                     '\n\n## Lists'
+                     '\n\n### Unordered\n\n* Item 1\n* Item 2\n  * Sub-item 2.1'
+                     '\n  * Sub-item 2.2'
+                     '\n\n### Ordered\n\n1. First item\n2. Second item'
+                     '\n   1. Sub-item 2.1\n   '
+                     '2. Sub-item 2.2\n\n## Links\n\n[OpenAI](https://www.openai.com/)'
+                     '\n\n## Images'
+                     '\n\n![Alt text](https://www.example.com/image.jpg)'
+                     '\n\n## Blockquotes\n\n'
+                     '> This is a blockquote.',
+                source='',
+                metadata={'test': '1'},
+                document_id='test_document_1'),
+
             KBDocChunk(id='test_document_1_3',
-                       text='~~Strikethrough text~~\n\n# Another first level header\ntext after first level header\n\n## Lists\n\n### Unordered\n\n* Item 1\n* Item 2\n  * Sub-item 2.1\n  * Sub-item 2.2\n\n### Ordered',  # noqa: E501
+                       text='## long text',
+                       source='',
                        metadata={'test': '1'},
                        document_id='test_document_1'),
+
             KBDocChunk(id='test_document_1_4',
-                       text='# Another first level header\ntext after first level header\n\n## Lists\n\n### Unordered\n\n* Item 1\n* Item 2\n  * Sub-item 2.1\n  * Sub-item 2.2\n\n### Ordered\n\n1. First item\n2. Second item\n   1. Sub-item 2.1\n   2. Sub-item 2.2',  # noqa: E501
+                       text='In a sleepy village, Lay a hidden secret. '
+                            'Anna found a map, Old, torn, and '
+                            'creased. It marked a spot, Beneath the old oak. '
+                            'With shovel in hand, Anna '
+                            'began to dig. Hours turned to minutes, '
+                            'Then, a chest appeared. Golden light '
+                            'spilled out, The village\'s lost lore. '
+                            'Inside, not gold, But memories and '
+                            'tales. Of',
+                       source='',
                        metadata={'test': '1'},
                        document_id='test_document_1'),
+
             KBDocChunk(id='test_document_1_5',
-                       text='## Lists\n\n### Unordered\n\n* Item 1\n* Item 2\n  * Sub-item 2.1\n  * Sub-item 2.2\n\n### Ordered\n\n1. First item\n2. Second item\n   1. Sub-item 2.1\n   2. Sub-item 2.2\n\n## Links\n\n[OpenAI](https://www.openai.com/)\n\n## Images\n\n![Alt text](https://www.example.com/image.jpg)\n\n## Blockquotes',  # noqa: E501
+                       text='lost lore. Inside, not gold, '
+                            'But memories and tales. '
+                            'Of brave ancestors, And '
+                            'magical whales.',
+                       source='',
                        metadata={'test': '1'},
                        document_id='test_document_1'),
+
             KBDocChunk(id='test_document_1_6',
-                       text="### Ordered\n\n1. First item\n2. Second item\n   1. Sub-item 2.1\n   2. Sub-item 2.2\n\n## Links\n\n[OpenAI](https://www.openai.com/)\n\n## Images\n\n![Alt text](https://www.example.com/image.jpg)\n\n## Blockquotes\n\n> This is a blockquote.\n\n## Inline code\n\nHere's some inline `code`.\n\n## Code blocks",  # noqa: E501
+                       text="Inside, not gold, But memories and tales."
+                            "\nOf brave ancestors, And magical "
+                            "whales.\n\nAnna shared the stories, Under stars so bright."
+                            "\nThe village united, "
+                            "Bathed in tales' light.",
+                       source='',
                        metadata={'test': '1'},
                        document_id='test_document_1'),
+
             KBDocChunk(id='test_document_1_7',
-                       text='## Blockquotes\n\n> This is a blockquote.\n\n## Inline code\n\nHere\'s some inline `code`.\n\n## Code blocks\n\n```python\ndef hello_world():\n    print("Hello, world!")\n```\n## table\na | b | c\n--- | --- | ---\n1 | 2 | 3',  # noqa: E501
+                       text="## Inline code\n\nHere's some inline `code`."
+                            "\n\n## Code blocks\n\n"
+                            "```python\ndef hello_world():"
+                            "\n    print(\"Hello, world!\")"
+                            "\n```\n## table"
+                            "\na | b | c\n--- | --- | ---\n1 | 2 | 3",
+                       source='',
                        metadata={'test': '1'},
                        document_id='test_document_1'),
+
             KBDocChunk(id='test_document_3_0',
                        text='# short markdown\nmarkdown is short',
+                       source='',
                        metadata={'test': '2'},
-                       document_id='test_document_3')]
+                       document_id='test_document_3')
+        ]
+        return chunks
