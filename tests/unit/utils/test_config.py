84cd3c3dd421de7d0508b7bb4becb2a4b994242d
ilai <ilai@pinecone.io>
2023-10-24 14:29:55 +0300

    [tests] Moved config defaults test to system tests
    Since this test instantiates acutual objects (namely `OpenAILLM`), it needs to have actual API keys etc.
    This shouldn't be part of the unit tests

--- tests/unit/utils/test_config.py
+++ tests/unit/utils/test_config.py
@@ -2,17 +2,10 @@
 import os
 
 import pytest
-import yaml
 
-from resin.chat_engine import ChatEngine
-from resin.context_engine import ContextEngine
-from resin.knowledge_base import KnowledgeBase
 from ._stub_classes import (BaseStubChunker, StubChunker, StubKB,
                             BaseStubContextEngine, StubOtherChunker, StubContextBuilder,
                             StubContextEngine, BaseStubKB)
-
-
-DEFAULT_COFIG_PATH = 'config/config.yaml'
 
 
 def test_list_supported_classes():
@@ -337,46 +330,3 @@ def test_init_complex_missing_mandatory_dependency():
     with pytest.raises(TypeError) as e:
         StubContextEngine()
     assert 'knowledge_base' in str(e.value)
-
-
-@pytest.fixture(scope='module')
-def temp_index_name():
-    index_name_before = os.getenv("INDEX_NAME", None)
-
-    os.environ["INDEX_NAME"] = "temp_index"
-    yield "temp_index"
-
-    if index_name_before is None:
-        del os.environ["INDEX_NAME"]
-    else:
-        os.environ["INDEX_NAME"] = index_name_before
-
-
-def test_default_config_matches_code_defaults(temp_index_name):
-    with open(DEFAULT_COFIG_PATH) as f:
-        default_config = yaml.safe_load(f)
-    chat_engine_config = default_config['chat_engine']
-
-    loaded_chat_engine = ChatEngine.from_config(chat_engine_config)
-    default_kb = KnowledgeBase(index_name=temp_index_name)
-    default_context_engine = ContextEngine(default_kb)
-    default_chat_engine = ChatEngine(default_context_engine)
-
-    def assert_identical_components(loaded_component, default_component):
-        assert type(loaded_component) == type(default_component)  # noqa: E721
-        if not loaded_component.__module__.startswith("resin"):
-            return
-
-        for key, value in default_component.__dict__.items():
-            assert hasattr(loaded_component, key), (
-                f"Missing attribute {key} in {type(loaded_component)}"
-            )
-            if hasattr(value, '__dict__'):
-                assert_identical_components(getattr(loaded_component, key), value)
-            else:
-                assert getattr(loaded_component, key) == value, (
-                    f"Attribute {key} in {type(loaded_component)} is {value} in code "
-                    f"but {getattr(loaded_component, key)} in config"
-                )
-
-    assert_identical_components(loaded_chat_engine, default_chat_engine)
