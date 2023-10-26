1a980cac7f8cd2743c014ecfc9ee99e74aa00048
Amnon Catav <catav.amnon1@gmail.com>
2023-10-26 16:07:21 +0300

    add test case

--- tests/unit/tokenizer/test_tokenizer_singleton.py
+++ tests/unit/tokenizer/test_tokenizer_singleton.py
@@ -3,6 +3,10 @@ from canopy.tokenizer import Tokenizer
 from ..stubs.stub_tokenizer import StubTokenizer
 
 
+class StubChildTokenizer(StubTokenizer):
+    pass
+
+
 @pytest.fixture
 def reset_tokenizer_singleton():
     before = Tokenizer._tokenizer_instance.__class__
@@ -18,11 +22,21 @@ def test_tokenizer_init(reset_tokenizer_singleton):
     assert Tokenizer._initialized is True
 
 
-def test_tokenizer_init_already_initialized(reset_tokenizer_singleton):
+def test_tokenizer_init_already_initialized_same_class(reset_tokenizer_singleton):
     Tokenizer.initialize(StubTokenizer, message_overhead=10)
+    tokenizer = Tokenizer()
     assert isinstance(Tokenizer._tokenizer_instance, StubTokenizer)
     assert Tokenizer._initialized is True
     assert Tokenizer._tokenizer_instance._message_overhead == 10
+    assert tokenizer._tokenizer_instance._message_overhead == 10
+
+
+def test_tokenizer_init_already_initialized_different_class(reset_tokenizer_singleton):
+    Tokenizer.initialize(StubChildTokenizer, message_overhead=10)
+    tokenizer = Tokenizer()
+    assert isinstance(Tokenizer._tokenizer_instance, StubChildTokenizer)
+    assert Tokenizer._initialized is True
+    assert isinstance(tokenizer._tokenizer_instance, StubChildTokenizer)
 
 
 def test_tokenizer_init_invalid_same_class(reset_tokenizer_singleton):
