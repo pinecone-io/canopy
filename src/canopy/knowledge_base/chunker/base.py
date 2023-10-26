fe5941e934fcead7b300bb3b543edce821e09cf1
Roy Miara <miararoy@gmail.com>
2023-10-26 23:22:44 +0300

    docstring base chunker

--- src/canopy/knowledge_base/chunker/base.py
+++ src/canopy/knowledge_base/chunker/base.py
@@ -9,10 +9,24 @@ from canopy.utils.config import ConfigurableMixin
 class Chunker(ABC, ConfigurableMixin):
 
     """
-    BaseChunker is an abstract class that defines the interface for a chunker.
+    Base class for chunkers. Chunkers take a document (id, text, ...) 
+    and return a list of KBDocChunks  (id, text, document_id, ...)
+
+    Chunker is an abstract class that must be subclassed to be used, also, it extends ConfigurableMixin
+    which means that every subclass of Chunker could be referenced by a name and configured in a config file.
     """
 
     def chunk_documents(self, documents: List[Document]) -> List[KBDocChunk]:
+        """
+        chunk_documents takes a list of documents and returns a list of KBDocChunks
+        this method is just a wrapper around chunk_single_document that can be used to chunk a list of documents.
+
+        Args:
+            documents (List[Document]): list of documents
+
+        Returns:
+            List[KBDocChunk]: list of chunks KBDocChunks
+        """
         chunks: List[KBDocChunk] = []
         for doc in documents:
             chunks.extend(self.chunk_single_document(doc))
@@ -26,6 +40,16 @@ class Chunker(ABC, ConfigurableMixin):
 
     @abstractmethod
     def chunk_single_document(self, document: Document) -> List[KBDocChunk]:
+        """
+        chunk_single_document takes a document and returns a list of KBDocChunks, this is the main method
+        that must be implemented by every subclass of Chunker
+
+        Args:
+            documents (List[Document]): list of documents
+
+        Returns:
+            List[KBDocChunk]: list of chunks KBDocChunks
+        """
         pass
 
     @abstractmethod
