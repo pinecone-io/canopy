69ee75807540b568be043382fd9542c170be89e0
Roy Miara <miararoy@gmail.com>
2023-10-30 20:01:59 +0200

    dense encoder

--- src/canopy/knowledge_base/reranker/reranker.py
+++ src/canopy/knowledge_base/reranker/reranker.py
@@ -6,6 +6,13 @@ from canopy.utils.config import ConfigurableMixin
 
 
 class Reranker(ABC, ConfigurableMixin):
+    """
+    Abstract class for rerankers. Rerankers take a list of KBQueryResult and return a list of KBQueryResult,
+    where the results are reranked according to the reranker logic.
+    Reranker is an abstract class that must be subclassed to be used,
+    also, it extends ConfigurableMixin which means that every subclass of Reranker could be referenced by a name
+    and configured in a config file.
+    """  # noqa: E501
 
     @abstractmethod
     def rerank(self, results: List[KBQueryResult]) -> List[KBQueryResult]:
@@ -17,7 +24,20 @@ class Reranker(ABC, ConfigurableMixin):
 
 
 class TransparentReranker(Reranker):
+    """
+    Transparent reranker that does nothing, it just returns the results as is. This is the default reranker.
+    The TransparentReranker is used as a placeholder for future development "forcing" every result set to be reranked.
+    """  # noqa: E501
     def rerank(self, results: List[KBQueryResult]) -> List[KBQueryResult]:
+        """
+        Returns the results as is.
+
+        Args:
+            results: A list of KBQueryResult to rerank.
+        
+        Returns:
+            results: A list of KBQueryResult, same as the input.
+        """  # noqa: E501
         return results
 
     async def arerank(self, results: List[KBQueryResult]) -> List[KBQueryResult]:
