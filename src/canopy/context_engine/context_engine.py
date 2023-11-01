40c3cf2c803cce02c5da2fa75ea9b92bf32f0869
Amnon Catav <catav.amnon1@gmail.com>
2023-11-01 09:34:02 +0200

    lint

--- src/canopy/context_engine/context_engine.py
+++ src/canopy/context_engine/context_engine.py
@@ -26,24 +26,23 @@ class BaseContextEngine(ABC, ConfigurableMixin):
 class ContextEngine(BaseContextEngine):
     """
     ContextEngine is responsible for providing context to the LLM, given a set of search queries.
-    
+
     Once called with a set of queries, the ContextEngine will go through the following steps:
     1. Query the knowledge base for relevant documents
     2. Build a context from the documents retrieved that can be injected into the LLM prompt
-    
+
     The context engine considers token budgeting when building the context, and tries to maximize the amount of relevant information that can be provided to the LLM within the token budget.
-    
+
     To create a context engine, you must provide a knowledge base and optionally a context builder.
-    
+
     Example:
     >>> from canopy.context_engine import ContextEngine
     >>> from canopy.models.data_models import Query
     >>> context_engine = ContextEngine(knowledge_base=knowledge_base)
     >>> context_engine.query(Query(text="What is the capital of France?"), max_context_tokens=1000)
-    
+
     To create a knowledge base, see the documentation for the knowledge base module (canopy.knowledge_base.knowledge_base).
     """  # noqa: E501
-
 
     _DEFAULT_COMPONENTS = {
         'knowledge_base': KnowledgeBase,
@@ -85,14 +84,14 @@ class ContextEngine(BaseContextEngine):
     def query(self, queries: List[Query], max_context_tokens: int, ) -> Context:
         """
         Query the knowledge base for relevant documents and build a context from the retrieved documents that can be injected into the LLM prompt.
-        
+
         Args:
             queries: A list of queries to use for retrieving documents from the knowledge base
             max_context_tokens: The maximum number of tokens to use for the context
-        
+
         Returns:
             A Context object containing the retrieved documents and metadata
-            
+
         Example:
         >>> from canopy.context_engine import ContextEngine
         >>> from canopy.models.data_models import Query
