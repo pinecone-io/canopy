08a7f2343da539bcf149fc8f4c5fe5387c833924
ilai <ilai@pinecone.io>
2023-11-05 12:12:00 +0200

    [app] Fix description
    Per Nathan and Byron's feedback

--- src/canopy_server/__init__.py
+++ src/canopy_server/__init__.py
@@ -6,8 +6,9 @@ Canopy provides a configurable built-in server so you can effortlessly deploy a 
 ## Prerequisites
 
 ### Pinecone API key
-To get Pinecone free trial API key and environment register or log into your Pinecone account in the console (https://app.pinecone.io/). You can access your API key from the "API Keys" section in the sidebar of your dashboard, and find the environment name next to it.
+If you don't have a Pinecone account, you can sign up for a free Starter plan at https://www.pinecone.io/.
+To find your Pinecone API key and environment log into Pinecone console (https://app.pinecone.io/). You can access your API key from the "API Keys" section in the sidebar of your dashboard, and find the environment name next to it.
 
 ### OpenAI API key
-You can find your free trial OpenAI API key https://platform.openai.com/account/api-keys. You might need to login or register to OpenAI services.
+You can find your free trial OpenAI API key https://platform.openai.com/account/api-keys. You might need to log in or register for OpenAI services.
 """  # noqa: E501
