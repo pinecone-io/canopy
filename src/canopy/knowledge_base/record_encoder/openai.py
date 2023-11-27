c4f98fa8e31da1532a33bbdf9e9a9589d61cbb9d
Michael Anckaert <michael.anckaert@sinax.be>
2023-11-27 10:43:28 +0100

    Use AzureOpenAIEncoder

--- src/canopy/knowledge_base/record_encoder/openai.py
+++ src/canopy/knowledge_base/record_encoder/openai.py
@@ -1,5 +1,5 @@
 from typing import List
-from pinecone_text.dense.openai_encoder import OpenAIEncoder
+from pinecone_text.dense.openai_encoder import OpenAIEncoder, AzureOpenAIEncoder
 from canopy.knowledge_base.models import KBDocChunk, KBEncodedDocChunk, KBQuery
 from canopy.knowledge_base.record_encoder.dense import DenseRecordEncoder
 from canopy.models.data_models import Query
@@ -28,6 +28,52 @@ class OpenAIRecordEncoder(DenseRecordEncoder):
             **kwargs: Additional arguments to pass to the underlying `pinecone-text. OpenAIEncoder`.
         """  # noqa: E501
         encoder = OpenAIEncoder(model_name, **kwargs)
+        super().__init__(dense_encoder=encoder, batch_size=batch_size)
+
+    def encode_documents(self, documents: List[KBDocChunk]) -> List[KBEncodedDocChunk]:
+        """
+        Encode a list of documents, takes a list of KBDocChunk and returns a list of KBEncodedDocChunk.
+
+        Args:
+            documents: A list of KBDocChunk to encode.
+
+        Returns:
+            encoded chunks: A list of KBEncodedDocChunk, with the `values` field populated by the generated embeddings vector.
+        """  # noqa: E501
+        return super().encode_documents(documents)
+
+    async def _aencode_documents_batch(self,
+                                       documents: List[KBDocChunk]
+                                       ) -> List[KBEncodedDocChunk]:
+        raise NotImplementedError
+
+    async def _aencode_queries_batch(self, queries: List[Query]) -> List[KBQuery]:
+        raise NotImplementedError
+
+
+class AzureOpenAIRecordEncoder(DenseRecordEncoder):
+    """
+    OpenAIRecordEncoder is a type of DenseRecordEncoder that uses the OpenAI `embeddings` API.
+    The implementation uses the `OpenAIEncoder` class from the `pinecone-text` library.
+    For more information about see: https://github.com/pinecone-io/pinecone-text
+
+    """  # noqa: E501
+
+    def __init__(self,
+                 *,
+                 model_name: str = "text-embedding-ada-002",
+                 batch_size: int = 400,
+                 **kwargs):
+        """
+        Initialize the OpenAIRecordEncoder
+
+        Args:
+            model_name: The name of the OpenAI embeddings model to use for encoding. See https://platform.openai.com/docs/models/embeddings
+            batch_size: The number of documents or queries to encode at once.
+                        Defaults to 400.
+            **kwargs: Additional arguments to pass to the underlying `pinecone-text. OpenAIEncoder`.
+        """  # noqa: E501
+        encoder = AzureOpenAIEncoder(model_name, **kwargs)
         super().__init__(dense_encoder=encoder, batch_size=batch_size)
 
     def encode_documents(self, documents: List[KBDocChunk]) -> List[KBEncodedDocChunk]:
