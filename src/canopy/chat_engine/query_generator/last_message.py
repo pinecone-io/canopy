d3a1df65b6ee14da4c82faf0cbf518b23b75195e
Izel Levy <izel@pinecone.io>
2023-12-10 16:16:30 +0200

    Take only user messages

--- src/canopy/chat_engine/query_generator/last_message.py
+++ src/canopy/chat_engine/query_generator/last_message.py
@@ -1,15 +1,16 @@
 from typing import List
 
 from canopy.chat_engine.query_generator import QueryGenerator
-from canopy.models.data_models import Messages, Query
+from canopy.models.data_models import Messages, Query, Role
 
 
 class LastMessageQueryGenerator(QueryGenerator):
     """
-        Returns the last message as a query without running any LLMs. This can be
+        Returns the last user message as a query without running any LLMs. This can be
         considered as the most basic query generation. Please use other query generators
         for more accurate results.
     """
+
     def generate(self,
                  messages: Messages,
                  max_prompt_tokens: int) -> List[Query]:
@@ -17,10 +18,13 @@ class LastMessageQueryGenerator(QueryGenerator):
             max_prompt_token is dismissed since we do not consume any token for
             generating the queries.
         """
-        if len(messages) == 0:
-            raise ValueError("Passed chat history does not contain any messages."
-                             " Please include at least one message in the history.")
-        return [Query(text=messages[-1].content)]
+        user_messages = [message for message in messages if message.role == Role.USER]
+
+        if len(user_messages) == 0:
+            raise ValueError("Passed chat history does not contain any user messages."
+                             " Please include at least one user message in the history.")
+
+        return [Query(text=user_messages[-1].content)]
 
     async def agenerate(self,
                         messages: Messages,
--- tests/unit/query_generators/test_last_message_query_generator.py
+++ tests/unit/query_generators/test_last_message_query_generator.py
@@ -1,13 +1,21 @@
 import pytest
 
 from canopy.chat_engine.query_generator import LastMessageQueryGenerator
-from canopy.models.data_models import UserMessage, Query
+from canopy.models.data_models import UserMessage, Query, AssistantMessage, Role
+
+
+@pytest.fixture
+def sample_user_messages():
+    return [
+        UserMessage(content="What is photosynthesis?")
+    ]
 
 
 @pytest.fixture
 def sample_messages():
     return [
-        UserMessage(content="What is photosynthesis?")
+        UserMessage(content="What is photosynthesis?"),
+        AssistantMessage(content="Oh! I don't know.")
     ]
 
 
@@ -16,19 +24,31 @@ def query_generator():
     return LastMessageQueryGenerator()
 
 
-def test_generate(query_generator, sample_messages):
-    expected = [Query(text=sample_messages[-1].content)]
-    actual = query_generator.generate(sample_messages, 0)
+def test_generate(query_generator, sample_user_messages):
+    expected = [Query(text=sample_user_messages[-1].content)]
+    actual = query_generator.generate(sample_user_messages, 0)
     assert actual == expected
 
 
 @pytest.mark.asyncio
-async def test_agenerate(query_generator, sample_messages):
-    expected = [Query(text=sample_messages[-1].content)]
-    actual = await query_generator.agenerate(sample_messages, 0)
+async def test_agenerate(query_generator, sample_user_messages):
+    expected = [Query(text=sample_user_messages[-1].content)]
+    actual = await query_generator.agenerate(sample_user_messages, 0)
+    assert actual == expected
+
+
+def test_generate_with_user_and_assistant(query_generator, sample_messages):
+    last_user_message = next(message for message in sample_messages if message.role == Role.USER)
+    expected = [Query(text=last_user_message.content)]
+    actual = query_generator.generate(sample_messages, 0)
     assert actual == expected
 
 
 def test_generate_fails_with_empty_history(query_generator):
     with pytest.raises(ValueError):
         query_generator.generate([], 0)
+
+
+def test_generate_fails_with_no_user_message(query_generator):
+    with pytest.raises(ValueError):
+        query_generator.generate([AssistantMessage(content="Hi! How can I help you?")], 0)
