16759c67e5ca36ad58f0cb2eb56325b61d6c6c57
Izel Levy <izel@pinecone.io>
2023-12-10 16:39:40 +0200

    Fix sample messages

--- tests/unit/query_generators/test_last_message_query_generator.py
+++ tests/unit/query_generators/test_last_message_query_generator.py
@@ -5,7 +5,7 @@ from canopy.models.data_models import UserMessage, Query, AssistantMessage
 
 
 @pytest.fixture
-def sample_user_messages():
+def sample_messages():
     return [
         UserMessage(content="What is photosynthesis?")
     ]
@@ -16,16 +16,16 @@ def query_generator():
     return LastMessageQueryGenerator()
 
 
-def test_generate(query_generator, sample_user_messages):
-    expected = [Query(text=sample_user_messages[-1].content)]
-    actual = query_generator.generate(sample_user_messages, 0)
+def test_generate(query_generator, sample_messages):
+    expected = [Query(text=sample_messages[-1].content)]
+    actual = query_generator.generate(sample_messages, 0)
     assert actual == expected
 
 
 @pytest.mark.asyncio
-async def test_agenerate(query_generator, sample_user_messages):
-    expected = [Query(text=sample_user_messages[-1].content)]
-    actual = await query_generator.agenerate(sample_user_messages, 0)
+async def test_agenerate(query_generator, sample_messages):
+    expected = [Query(text=sample_messages[-1].content)]
+    actual = await query_generator.agenerate(sample_messages, 0)
     assert actual == expected
 
 
