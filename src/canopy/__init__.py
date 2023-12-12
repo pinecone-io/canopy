62fc91ebf35ec590e4b32cf6c287d514841668fd
ilai <ilai@pinecone.io>
2023-12-12 12:52:44 +0200

    Added option to ignore specific warnings, or ignore warnings from a specific module
    In addition, the `transformers` module is very insisstent, so I added their dedicated mechanism for silencing warnings

--- src/canopy/__init__.py
+++ src/canopy/__init__.py
@@ -1,4 +1,23 @@
 import importlib.metadata
+import warnings
+import logging
+import os
 
 # Taken from https://stackoverflow.com/a/67097076
 __version__ = importlib.metadata.version("canopy-sdk")
+
+IGNORED_WARNINGS = [
+]
+
+IGNORED_WARNING_IN_MODULES = [
+    "transformers",
+]
+
+for warning in IGNORED_WARNINGS:
+    warnings.filterwarnings("ignore", message=warning)
+for module in IGNORED_WARNING_IN_MODULES:
+    warnings.filterwarnings("ignore", module=module)
+    logging.getLogger(module).setLevel(logging.ERROR)
+
+# Apparently, `transformers` has its own logging system, and needs to be silenced separately # noqa: E501
+os.environ["TRANSFORMERS_VERBOSITY"] = "error"
