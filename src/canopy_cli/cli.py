bd1c0398519cf71c428250a1864d223dd7c111b5
igiloh-pinecone <118673156+igiloh-pinecone@users.noreply.github.com>
2023-12-13 17:28:09 +0200

    Slightly improve error handling for external providers (#220)
    * [kb] Improve error handling for OpenAIRecord encoder
    
    Catch specific OpenAI errors and re-raise with a clear message
    
    * [kb] Improve the error in case of failing to infer dimensionality
    
    * Added option to ignore specific warnings, or ignore warnings from a specific module
    
    In addition, the `transformers` module is very insisstent, so I added their dedicated mechanism for silencing warnings
    
    * Bug fix - infering dimension incorrectly
    
    * [CLI] Removed dedicated error message for OpenAI auth problem
    
    Instead, individual RecordEncoders and LLMs would need to raise their own errors
    
    * [server] Call health_check() on startup
    
    To detect errors early
    
    * [cli] Bug fix - validate function did not connect to KB
    
    * [LLM] Explicit error when initializing and when using OpenAI components - both LLM and RecordEncoder
    
    * [kb] Slightly better solution for error handling
    
    Catch the error in RecordEncoder base class, then format for each inhertor differently
    
    * linter
    
    * revert accidental commit
    
    * [test] KB test - improved dimension infer testing
    
    1. Needed to change after code was changed.
    2. Added a few more test cases
    3. Added more assertions on the naive create()
    
    * [test] Fix LLM tests after error handling
    
    Some error types changed
    
    * [cli] Improve error message in case of index already exists
    
    The CLI shouldn't mention `delete_index()`
    
    * [CLI] Improve ChatEngine validate - use less tokens
    
    Sending just "hello" without any limitations can use many tokens
    
    * [LLM] Explicit error for function calling failed
    
    Based on feedback in PR #220

--- src/canopy_cli/cli.py
+++ src/canopy_cli/cli.py
@@ -22,7 +22,7 @@ from canopy.knowledge_base import KnowledgeBase
 from canopy.knowledge_base import connect_to_pinecone
 from canopy.knowledge_base.chunker import Chunker
 from canopy.chat_engine import ChatEngine
-from canopy.models.data_models import Document
+from canopy.models.data_models import Document, UserMessage
 from canopy.tokenizer import Tokenizer
 from canopy_cli.data_loader import (
     load_from_path,
@@ -43,12 +43,6 @@ load_dotenv()
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 DEFAULT_SERVER_URL = f"http://localhost:8000/{API_VERSION}"
 spinner = Spinner()
-
-OPENAI_AUTH_ERROR_MSG = (
-    "Failed to connect to OpenAI, please make sure that the OPENAI_API_KEY "
-    "environment variable is set correctly.\n"
-    "Please visit https://platform.openai.com/account/api-keys for more details"
-)
 
 
 def check_server_health(url: str):
@@ -140,9 +134,15 @@ def _validate_chat_engine(config_file: Optional[str]):
     config = _read_config_file(config_file)
     Tokenizer.initialize()
     try:
-        ChatEngine.from_config(config.get("chat_engine", {}))
-    except openai.OpenAIError:
-        raise CLIError(OPENAI_AUTH_ERROR_MSG)
+        # If the server itself will fail, we can't except the error, since it's running
+        # in a different process. Try to load and run the ChatEngine so we can catch
+        # any errors and print a nice message.
+        chat_engine = ChatEngine.from_config(config.get("chat_engine", {}))
+        chat_engine.max_generated_tokens = 5
+        chat_engine.context_engine.knowledge_base.connect()
+        chat_engine.chat(
+            [UserMessage(content="This is a health check. Are you alive? Be concise")]
+        )
     except Exception as e:
         msg = f"Failed to initialize Canopy server. Reason:\n{e}"
         if config_file:
@@ -229,9 +229,15 @@ def new(index_name: str, config: Optional[str]):
     with spinner:
         try:
             kb.create_canopy_index()
-        # TODO: kb should throw a specific exception for each case
+        # TODO: kb should throw a specific exception for failure
         except Exception as e:
-            msg = f"Failed to create a new index. Reason:\n{e}"
+            already_exists_str = f"Index {kb.index_name} already exists"
+            if isinstance(e, RuntimeError) and already_exists_str in str(e):
+                msg = (f"{already_exists_str}, please use a different name."
+                       f"If you wish to delete the index, log in to Pinecone's "
+                       f"Console: https://app.pinecone.io/")
+            else:
+                msg = f"Failed to create a new index. Reason:\n{e}"
             raise CLIError(msg)
     click.echo(click.style("Success!", fg="green"))
     os.environ["INDEX_NAME"] = index_name
@@ -303,8 +309,8 @@ def upsert(index_name: str,
     kb_config = _load_kb_config(config)
     try:
         kb = KnowledgeBase.from_config(kb_config, index_name=index_name)
-    except openai.OpenAIError:
-        raise CLIError(OPENAI_AUTH_ERROR_MSG)
+    except Exception as e:
+        raise CLIError(str(e))
 
     try:
         kb.connect()
