7844f31efa6342a29c7b245805e60555223b22f8
kYLe <kyle@anyscale.com>
2023-12-13 01:53:06 -0600

    Add Anyscale Embedding model support (#198)
    * Add Anyscale Embedding model support
    
    * fixed flake8 error DOC301, no docstring in __init__()
    
    * Added comment for list of supported LLM for Anyscale
    
    * [cli] Batch size should be dynamic, determined by the chose encoder
    
    We have a hacky workaround in CLI to select the batch_size of documents by the expected batch_size of underlying chunks to encode.
    However this mechanism used a hardcoded number, instead of using the actual Encoder's batch_size
    
    ---------
    
    Co-authored-by: ilai <ilai@pinecone.io>

--- src/canopy/knowledge_base/record_encoder/__init__.py
+++ src/canopy/knowledge_base/record_encoder/__init__.py
@@ -2,3 +2,4 @@ from .base import RecordEncoder
 from .cohere import CohereEncoder
 from .dense import DenseRecordEncoder
 from .openai import OpenAIRecordEncoder
+from .anyscale import AnyscaleRecordEncoder
