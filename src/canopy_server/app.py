fa9eafc55b2a6cb2318551c70feb6f1c9163fa91
Amnon Catav <amnoncatav1@gmail.com>
2023-12-18 16:03:43 +0200

    fix app

--- src/canopy_server/app.py
+++ src/canopy_server/app.py
@@ -238,7 +238,7 @@ async def health_check() -> HealthStatus:
 
     try:
         msg = UserMessage(content="This is a health check. Are you alive? Be concise")
-        await run_in_threadpool(llm.chat_completion, messages=[msg], max_tokens=5)
+        await run_in_threadpool(llm.chat_completion, system_prompt="hi", chat_history=[msg], max_tokens=5)
     except Exception as e:
         err_msg = f"Failed to communicate with {llm.__class__.__name__}"
         logger.exception(err_msg)
