b7a403ec4199402e45e5be1a11157136994e0b72
Scot Mountenay <scott@canadiense.net>
2024-01-10 10:33:03 -0600

    Add tests for passing documents to cohere llm

--- tests/system/llm/test_cohere.py
+++ tests/system/llm/test_cohere.py
@@ -2,8 +2,12 @@ from unittest.mock import MagicMock
 
 import pytest
 from cohere.error import CohereAPIError
+from pydantic.error_wrappers import ValidationError
 
-from canopy.models.data_models import Role, MessageBase # noqa
+from canopy.models.data_models import Context, ContextContent, Role, MessageBase # noqa
+from canopy.context_engine.context_builder.stuffing import (
+    StuffingContextContent, ContextQueryResult, ContextSnippet
+)
 from canopy.models.api_models import ChatResponse, StreamingChatChunk # noqa
 from canopy.llm.cohere import CohereLLM  # noqa
 
@@ -55,6 +59,15 @@ def model_params_low_temperature():
 @pytest.fixture
 def cohere_llm(model_name):
     return CohereLLM(model_name=model_name)
+
+
+@pytest.fixture
+def unsupported_context():
+    class UnsupportedContextContent(ContextContent):
+        def to_text(self, **kwargs):
+            return ''
+
+    return Context(content=UnsupportedContextContent(), num_tokens=123)
 
 
 def test_init_with_custom_params():
@@ -121,7 +134,8 @@ def test_missing_messages(cohere_llm):
 
 def test_negative_max_tokens(cohere_llm, messages):
     with pytest.raises(CohereAPIError):
-        cohere_llm.chat_completion(chat_history=messages, max_tokens=-5, system_prompt='')
+        cohere_llm.chat_completion(
+            chat_history=messages, max_tokens=-5, system_prompt='')
 
 
 def test_chat_completion_api_failure_propagates(cohere_llm,
@@ -130,4 +144,56 @@ def test_chat_completion_api_failure_propagates(cohere_llm,
     cohere_llm._client.chat.side_effect = CohereAPIError("API call failed")
 
     with pytest.raises(CohereAPIError, match="API call failed"):
-        cohere_llm.chat_completion(chat_history=messages, system_prompt='')
+        cohere_llm.chat_completion(chat_history=messages, system_prompt="")
+
+
+def test_chat_completion_with_unsupported_context_engine(cohere_llm,
+                                                         messages,
+                                                         unsupported_context):
+    cohere_llm._client = MagicMock()
+
+    with pytest.raises(NotImplementedError):
+        cohere_llm.chat_completion(chat_history=messages,
+                                   system_prompt="",
+                                   context=unsupported_context)
+
+
+def test_chat_completion_with_stuffing_context_snippets(cohere_llm,
+                                                        messages):
+    cohere_llm._client = MagicMock()
+    content = StuffingContextContent(__root__=[
+            ContextQueryResult(query="", snippets=[
+                ContextSnippet(
+                    source="http://www.example.com/document",
+                    text="Document text",
+                )
+            ])
+    ])
+    stuffing_context = Context(
+        content=content,
+        num_tokens=123)
+
+    try:
+        cohere_llm.chat_completion(chat_history=messages,
+                                   system_prompt="",
+                                   context=stuffing_context)
+    except ValidationError:
+        # We want to test what was passed to the Cohere client, and don't
+        # care about the chat response object.
+        pass
+
+    cohere_llm._client.chat.assert_called_once_with(
+        model="command",
+        message='Hello, user. How can I assist you?',
+        chat_history=[{'role': 'USER', 'message': 'Hello, assistant.'}],
+        connectors=None,
+        documents=[
+            {
+                "source": "http://www.example.com/document",
+                "text": "Document text",
+            },
+        ],
+        preamble_override="",
+        stream=False,
+        max_tokens=None,
+    )
