2f821e4a61dfb5c5dc10129f0f500f202d06069d
Tom Aarsen <Cubiegamedev@gmail.com>
2024-01-24 16:10:31 +0100

    Make query_encoder_name & device arguments explicit

--- src/canopy/knowledge_base/record_encoder/sentence_transformers.py
+++ src/canopy/knowledge_base/record_encoder/sentence_transformers.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pinecone_text.dense import SentenceTransformerEncoder
 from canopy.knowledge_base.record_encoder.dense import DenseRecordEncoder
 
@@ -13,17 +14,30 @@ class SentenceTransformerRecordEncoder(DenseRecordEncoder):
     def __init__(self,
                  *,
                  model_name: str = "sentence-transformers/all-MiniLM-L6-v2",
+                 query_encoder_name: Optional[str] = None,
                  batch_size: int = 400,
-                 **kwargs):
+                 device: Optional[str] = None,
+                 **kwargs) -> None:
         """
         Initialize the SentenceTransformerRecordEncoder
 
         Args:
-            model_name: The name of the embedding model to use. See https://huggingface.co/models?library=sentence-transformers
+            model_name: The name of the embedding model to use for encoding documents.
+                        See https://huggingface.co/models?library=sentence-transformers
+                        for all possible Sentence Transformer models.
+            query_encoder_name: The name of the embedding model to use for encoding queries.
+                        See https://huggingface.co/models?library=sentence-transformers
                         for all possible Sentence Transformer models.
             batch_size: The number of documents or queries to encode at once.
                         Defaults to 400.
+            device: The local device to use for encoding, for example "cpu", "cuda" or "mps".
+                        Defaults to "cuda" if cuda is available, otherwise to "cpu".
             **kwargs: Additional arguments to pass to the underlying `pinecone-text.SentenceTransformerEncoder`.
         """  # noqa: E501
-        encoder = SentenceTransformerEncoder(model_name, **kwargs)
+        encoder = SentenceTransformerEncoder(
+            document_encoder_name=model_name,
+            query_encoder_name=query_encoder_name,
+            device=device,
+            **kwargs,
+        )
         super().__init__(dense_encoder=encoder, batch_size=batch_size)
